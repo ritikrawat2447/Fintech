24fc5c80b693006e9a86a3ed84b0ab48094ffefd
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2026-08-06 20:13:58 +0530

    Intitial commit

---
+++ src/test/java/com/extradict/fintechapi/FintechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extradict.fintechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
