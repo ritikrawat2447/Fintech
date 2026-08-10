5a6366e6926bec830dc0d0a2f00c4782970a213a
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2026-08-10 17:33:06 +0000

    fix: removed /api/demo endpoint from permitAll and now reuires authentication

--- src/main/java/com/extradict/fintechapi/config/SecurityConfig.java
+++ src/main/java/com/extradict/fintechapi/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login").permitAll()
-                .requestMatchers("/actuator/**", "/api/health", "/api/demo").permitAll()
+                .requestMatchers("/actuator/**", "/api/health").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
