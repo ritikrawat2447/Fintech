bffc8caef3a17c9982a424b71923cd08539974f5
Ritik Rawat <75121026+ritikrawat2447@users.noreply.github.com>
2026-08-16 06:09:31 +0000

    fix: updated health controller test file

--- src/test/java/com/extradict/fintechapi/HealthControllerTest.java
+++ src/test/java/com/extradict/fintechapi/HealthControllerTest.java
@@ -1,5 +1,6 @@
 package com.extradict.fintechapi.controller;
 
+import com.extradict.fintechapi.filter.RateLimitFilter;
 import com.extradict.fintechapi.security.JwtAuthFilter;
 import com.extradict.fintechapi.security.JwtService;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,9 +27,13 @@ class HealthControllerTest {
     private JwtAuthFilter jwtAuthFilter;
 
     @MockBean
+    private RateLimitFilter rateLimitFilter;
+
+    @MockBean
     private UserDetailsService userDetailsService;
 
     @Test
+    @WithMockUser
     void shouldReturnHealthOk() throws Exception {
         mockMvc.perform(get("/api/health"))
                 .andExpect(status().isOk())
